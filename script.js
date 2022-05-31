277c0267dcda960f5ad1f4b56799276498835d79
MaksimovicOff <maximsannikovwork@gmail.com>
2022-05-31 17:23:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
 const todo = {
-  
     action(e) {
       const target = e.target;
       if (target.classList.contains('todo__action')) {
         const action = target.dataset.todoAction;
         const elemItem = target.closest('.todo__item');
+        
         if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
           elemItem.remove();
         } else {
           elemItem.dataset.todoState = action; 
-          $("button.rea_all").on('click', function(){
-    
-            for (let i = 0; i < 1; i++) {
-              document.querySelector('.todo__item').setAttribute('data-todo-state', 'completed');
-              save();
-            }
-            
-          })
         }
         this.save();
-      } else if (target.classList.contains('add')) {
+      } else if (target.classList.contains('create')) {
+        console.log('rrr');
+        this.add();
+        this.save();
+      } else if (target.classList.contains('save')) {
+        console.log('rrr');
         this.add();
         this.save();
       }
     },
     add() {
-      const elemText = document.querySelector('.input_text');
-      if (elemText.disabled || !elemText.value.length) {
-        return;
-      }
-      document.querySelector('.todo__items').insertAdjacentHTML('beforeend', this.create(elemText.value));
+      const elemDesc = document.querySelector('.rrr');
+      const divik = document.createElement('div');
+      divik.classList.add('divik');
+      const spic = document.createElement('li');
+      spic.insertAdjacentHTML('beforeend', this.create1(elemDesc.value));
+      const elemNumber = document.querySelector('.number');
+      spic.insertAdjacentHTML('beforeend', this.create2(elemNumber.value));
+      const elemText = document.querySelector('.name');
+      divik.insertAdjacentHTML('afterbegin', this.create(elemText.value));
       elemText.value = '';
+      divik.appendChild(spic);
+      document.querySelector('.result').appendChild(divik);
+      divik.insertAdjacentHTML('beforeend', this.create3());
     },
     create(text) {
-    return `<div class="todo__item" data-todo-state="active">
-        <div class="todo__task">${text}</div>
-        <button class="todo__action todo__action_restore" data-todo-action="active">UNREADY</button>
-        <button class="todo__action todo__action_complete" data-todo-action="completed">READY</button>
-        <button class="todo__action todo__action_delete" data-todo-action="deleted">LOAD</button>
-        </div>`;
+      return `<span class="todo__task"><b>${text}</b></span><br>`;
+    },
+    create1(desc) {
+      return `<span>${desc}`;
+    },
+    create2(number) {
+      return `<span> - ${number}h</span><br>`;
+    },
+    create3() {
+      return `<button class="bts">Load</button>`;
     },
     init() {
       const fromStorage = localStorage.getItem('todo');
       if (fromStorage) {
-        document.querySelector('.todo__items').innerHTML = fromStorage;
+        document.querySelector('.result').innerHTML = fromStorage;
       }
-      document.querySelector('.todo__options').addEventListener('change', this.update);
+      // document.querySelector('.todo__options').addEventListener('change', this.update);
       document.addEventListener('click', this.action.bind(this));
     },
     update() {
       const option = document.querySelector('.todo__options').value;
       document.querySelector('.todo__items').dataset.todoOption = option;
-      document.querySelector('.input_text').disabled = option !== 'active';
+      document.querySelector('.todo__text').disabled = option !== 'active';
     },
     save() {
-      localStorage.setItem('todo', document.querySelector('.todo__items').innerHTML);
+      localStorage.setItem('todo', document.querySelector('.result').innerHTML);
     }
   }
-  $("button.rem_all").on('click', function(){
+  $("button.mmm").on('click', function(){
     localStorage.clear();
     document.querySelector('.todo__items').innerHTML = '';
   })
+  
   todo.init();
