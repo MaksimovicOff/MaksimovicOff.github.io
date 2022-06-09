a8346fe29d1fb520589df880c5e2d5d49e8c33ed
MaksimovicOff <maximsannikovwork@gmail.com>
2022-06-09 17:55:30 +0400

    Delete delet.js

--- delet.js
+++
@@ -1,9 +0,0 @@
-function delet(event) {
-      console.log(event.target);
-      const target = event.target;
-      const elemItem = target.closest('.bl');
-      if (target.classList.contains('stask_delete')) {
-        elemItem.remove();
-        console.log('adsd');
-      }
-    }
